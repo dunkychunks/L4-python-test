11e351a4d7a384de4232013b19dfd9461f540b67
dunkychunks <44877047+dunkychunks@users.noreply.github.com>
2023-11-04 15:40:04 +0000

    Added final changes for printthings module

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # L4-python-test
 Python Repo for L4 Class
+
+Our Helloe World project
--- printthings/__init__.py
+++ printthings/__init__.py
@@ -1,3 +1,5 @@
 # dunder = double underscore
 
 # dunder init (dunder also at the end) means we are telling python this is now a module
+
+print("Hello")
